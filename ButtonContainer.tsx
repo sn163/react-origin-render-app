4043593958fb9819ea6213df985b145c32e68873
sn163 <sonnynguyen163@gmail.com>
2022-04-02 16:40:28 -0700

    Completed Fetch & Abort functionality

--- ButtonContainer.tsx
+++ ButtonContainer.tsx
@@ -2,27 +2,55 @@ import React, {useState, useEffect} from 'react';
 let controller;
 
 export const ButtonContainer: React.FC<{}> = () => {
-  const [count, newCount] = useState(0)
+  const [count, setCount] = useState(0)
   const [loading, setLoading] = useState(false)
   const [cancel, setCancel] = useState(true)
   const [origin, setOrigin] = useState(() => {
-    const saved = localStorage.getItem(origin)
+    const saved = localStorage.getItem('origin')
+    const value = saved
+    return value || 'xx.xx.xxx.xxx'
   })
 
+  localStorage.clear()
   
 const handleClick = () => {
+  controller = new AbortController();
+  const signal = controller.signal
+  setCancel(false)
+  setLoading(true)
 
+  fetch('https://httpbin.org/delay/4', { signal })
+  .then(res => res.json())
+  .then(data => {
+    localStorage.setItem('origin', data.origin)
+    setOrigin(data.origin)
+    setLoading(false)
+    setCancel(true)
+  })
+  .catch(error => {
+    setLoading(false)
+    setCancel(true)
+    if (error.name === 'AbortError') {
+      console.log('Fetch canceled by user')
+    } else {
+      console.log('Fetch failed')
+    }
+  })
+}
 
-
-
-
+const handleCancel = () => {
+  setLoading(false)
+  controller.abort()
+  console.log('Fetch has been aborted')
 }
 
 
   return (
     <div>
-      <button onClick={handleClick} > Click me {loading && '(loading)'} </button>
-      <button disabled={cancel}> Cancel </button>
+      <button onClick={handleClick} disabled={loading}> Click me {loading && '(loading)'} </button>
+      <button onClick={handleCancel} disabled={cancel}> Cancel </button>
+      <p> Renders: {count} </p>
+      <p> Origin: {origin} </p>
       </div>
   
   )
