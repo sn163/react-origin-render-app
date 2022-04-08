10ea78dcbae9ae84206bc8f7e8ce42711939ca79
sn163 <sonnynguyen163@gmail.com>
2022-04-08 11:40:31 -0700

    added additional comments

--- ButtonContainer.tsx
+++ ButtonContainer.tsx
@@ -2,15 +2,19 @@ import React, {useState, useEffect, useRef} from 'react';
 let controller;
 
 export const ButtonContainer: React.FC<{}> = () => {
+  //initialize useRef's .current prop to the input of 2 & assign to the count variable to log our render counts
   const count = useRef(2)
   const [loading, setLoading] = useState(false)
   const [origin, setOrigin] = useState(() => {
+    //set the origin's default state to saved
     const saved = localStorage.getItem('origin')
-    const value = saved
-    return value || 'xx.xx.xxx.xxx'
+    //update the state to whether saved was able to access the origin data, it not then return the x string. 
+    return saved || 'xx.xx.xxx.xxx'
   })
   
+
   useEffect(()=> {
+    //on each rerender, increment the count's current property by 1
     count.current+=1
   })
   
@@ -18,10 +22,11 @@ export const ButtonContainer: React.FC<{}> = () => {
   //localStorage.clear()
   
 const handleClick = () => {
+  //create a new instance of the abort controller to to be consumed if the fetch is aborted
   controller = new AbortController();
   const signal = controller.signal
   setLoading(true)
-
+  //pass in the abort signal as an option inside the request to relate the signal and controller with the fetch
   fetch('https://httpbin.org/delay/4', { signal })
   .then(res => res.json())
   .then(data => {
@@ -41,9 +46,11 @@ const handleClick = () => {
 
 const handleCancel = () => {
   setLoading(false)
+  //if the cancel button is clicked, invoke controller's abort method to abort fetch request & consume the instance
   controller.abort()
   console.log('Fetch has been aborted')
 }
+
 
   return (
     <div>
